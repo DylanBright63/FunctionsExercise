9f39178d351fc8e8d0f6ff544a00d97f35204b0f
Student <Brightdylan93@gmail.com>
2023-04-17 09:05:09 -0500

    done

---
+++ script.js
@@ -0,0 +1,60 @@
+//1
+function fozzieBear(){
+    console.log(`Wocka Wocka!`);  
+} fozzieBear();
+
+//2 
+function beaker(speak) {
+    console.log(speak.repeat(4));
+} beaker(`Meep`);
+
+//3
+function muppetShow(a, b) {
+    if([a] in {a,b},`Muppet`);
+    if([b] in {a,b}, `Show`);
+    console.log("It's time to play the music. It's time to light the lights.");
+} muppetShow(`Muppet`, `Show`);
+
+
+//4
+function kermit() {
+    return `Kermit The Frog`;
+} kermit();
+console.log(kermit());
+
+
+//5
+function muppetShowSeasons(seasons) {
+    if (seasons === 5){
+        return true;
+    } else {
+        return false;}
+}  console.log(muppetShowSeasons(5))
+
+
+//6
+const manOrMuppet = function() {
+    console.log("Am I a man or am I a Muppet?");
+    } manOrMuppet();
+
+
+    //7
+rainbowConnection = () => `Someday we'll find it. The Rainbow Connection. The lovers, the dreamers and me`;
+console.log(rainbowConnection());
+
+//8
+No.
+
+//9
+yes.
+
+//10a
+const newMuppetMovies = [
+    `The Muppets`,
+    `Muppets Most Wanted`];
+
+
+//10b
+const upperMovies = newMuppetMovies.map(function(x){
+    return x.toUpperCase();
+});console.log(upperMovies);
